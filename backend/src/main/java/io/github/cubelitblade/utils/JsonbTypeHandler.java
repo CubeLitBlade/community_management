238e1b573e5ef8860a8a77a31caef1bb69969cbe
CubeLitBlade <135247075+CubeLitBlade@users.noreply.github.com>
2026-03-13 15:02:39 +0800

    Added the JsonbTypeHandler class to support processing of PostgreSQL's jsonb data type.

---
+++ backend/src/main/java/io/github/cubelitblade/utils/JsonbTypeHandler.java
@@ -0,0 +1,31 @@
+package io.github.cubelitblade.utils;
+
+import com.baomidou.mybatisplus.extension.handlers.Jackson3TypeHandler;
+import org.apache.ibatis.type.JdbcType;
+import org.apache.ibatis.type.MappedTypes;
+import org.postgresql.util.PGobject;
+
+import java.lang.reflect.Field;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+@MappedTypes({Object.class})
+public class JsonbTypeHandler extends Jackson3TypeHandler {
+    public JsonbTypeHandler(Class<?> type) {
+        super(type);
+    }
+
+    public JsonbTypeHandler(Class<?> type, Field field) {
+        super(type, field);
+    }
+
+    @Override
+    public void setNonNullParameter(PreparedStatement ps, int i, Object parameter, JdbcType jdbcType) throws SQLException {
+        if (ps != null) {
+            PGobject jsonObject = new PGobject();
+            jsonObject.setType("jsonb");
+            jsonObject.setValue(toJson(parameter));
+            ps.setObject(i, jsonObject);
+        }
+    }
+}
