1f8cd86057a4fba072121d120579fa4921de3df8
CubeLitBlade <135247075+CubeLitBlade@users.noreply.github.com>
2026-03-13 15:03:26 +0800

    Rework the Event class by replacing the type field with the EventType enumeration, updating the payload field's type handler to JsonbTypeHandler.

--- backend/src/main/java/io/github/cubelitblade/entity/Event.java
+++ backend/src/main/java/io/github/cubelitblade/entity/Event.java
@@ -1,11 +1,10 @@
 package io.github.cubelitblade.entity;
 
+import com.baomidou.mybatisplus.annotation.EnumValue;
 import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.extension.handlers.JacksonTypeHandler;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import com.baomidou.mybatisplus.annotation.TableName;
+import io.github.cubelitblade.utils.JsonbTypeHandler;
+import lombok.*;
 import tools.jackson.databind.JsonNode;
 
 import java.time.LocalDateTime;
@@ -13,17 +12,44 @@ import java.time.LocalDateTime;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
+@TableName(autoResultMap = true)
 public class Event {
+    @Getter
+    public enum EventType {
+        TEST_EVENT("test_event");
+
+        @EnumValue
+        private final String type;
+
+        EventType(String type) {
+            this.type = type;
+        }
+    }
+
+    @Getter
+    public enum EventStatus {
+        WAITING("waiting"),
+        PENDING("pending"),
+        RUNNING("running"),
+        SUCCEEDED("succeeded"),
+        FAILED("failed");
+
+        @EnumValue
+        private final String status;
+
+        EventStatus(String status) {
+            this.status = status;
+        }
+    }
+
     private Long id;
 
-    private String type;
+    private EventType type;
 
-    @TableField(typeHandler = JacksonTypeHandler.class)
+    @TableField(typeHandler = JsonbTypeHandler.class)
     private JsonNode payload;
 
-    @Builder.Default
-    private String status = "pending";
+    private EventStatus status;
 
     private Integer retryCount;
 
