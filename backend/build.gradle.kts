aab93720a11926316855b0ab0d036eac2638bb4e
CubeLitBlade <135247075+CubeLitBlade@users.noreply.github.com>
2026-03-15 16:06:01 +0800

    feat(dependencies): add spring-boot-docker-compose dependency

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -31,6 +31,7 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    developmentOnly("org.springframework.boot:spring-boot-docker-compose:4.1.0-M2")
 }
 
 tasks.test {
