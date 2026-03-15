949b53e5776ff1640b9d4f620f4a9730f03114e0
CubeLitBlade <135247075+CubeLitBlade@users.noreply.github.com>
2026-03-15 16:08:07 +0800

    refactor(schema): remove owner information

--- init_sql/schema.sql
+++ init_sql/schema.sql
@@ -20,7 +20,7 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- Name: update_updated_at_column(); Type: FUNCTION; Schema: public; Owner: user
+-- Name: update_updated_at_column(); Type: FUNCTION; Schema: public
 --
 
 CREATE FUNCTION public.update_updated_at_column() RETURNS trigger
@@ -32,15 +32,12 @@ BEGIN
 END;
 $$;
 
-
-ALTER FUNCTION public.update_updated_at_column() OWNER TO "user";
-
 SET default_tablespace = '';
 
 SET default_table_access_method = heap;
 
 --
--- Name: event; Type: TABLE; Schema: public; Owner: user
+-- Name: event; Type: TABLE; Schema: public
 --
 
 CREATE TABLE public.event (
@@ -55,74 +52,71 @@ CREATE TABLE public.event (
     updated_at timestamp(6) without time zone DEFAULT CURRENT_TIMESTAMP
 );
 
-
-ALTER TABLE public.event OWNER TO "user";
-
 --
--- Name: COLUMN event.id; Type: COMMENT; Schema: public; Owner: user
+-- Name: COLUMN event.id; Type: COMMENT; Schema: public
 --
 
 COMMENT ON COLUMN public.event.id IS '事件的唯一标识符。';
 
 
 --
--- Name: COLUMN event.type; Type: COMMENT; Schema: public; Owner: user
+-- Name: COLUMN event.type; Type: COMMENT; Schema: public
 --
 
 COMMENT ON COLUMN public.event.type IS '事件的类型，用于提供给 worker 分配任务。';
 
 
 --
--- Name: COLUMN event.payload; Type: COMMENT; Schema: public; Owner: user
+-- Name: COLUMN event.payload; Type: COMMENT; Schema: public
 --
 
 COMMENT ON COLUMN public.event.payload IS '事件所需的参数。';
 
 
 --
--- Name: COLUMN event.status; Type: COMMENT; Schema: public; Owner: user
+-- Name: COLUMN event.status; Type: COMMENT; Schema: public
 --
 
 COMMENT ON COLUMN public.event.status IS '事件的状态，合法值为 pending、succeed、failed 等。';
 
 
 --
--- Name: COLUMN event.retry_count; Type: COMMENT; Schema: public; Owner: user
+-- Name: COLUMN event.retry_count; Type: COMMENT; Schema: public
 --
 
 COMMENT ON COLUMN public.event.retry_count IS '事件重试次数。';
 
 
 --
--- Name: COLUMN event.error_msg; Type: COMMENT; Schema: public; Owner: user
+-- Name: COLUMN event.error_msg; Type: COMMENT; Schema: public
 --
 
 COMMENT ON COLUMN public.event.error_msg IS '事件产生错误时的错误消息。';
 
 
 --
--- Name: COLUMN event.created_at; Type: COMMENT; Schema: public; Owner: user
+-- Name: COLUMN event.created_at; Type: COMMENT; Schema: public
 --
 
 COMMENT ON COLUMN public.event.created_at IS '事件创建时的时间戳。';
 
 
 --
--- Name: COLUMN event.next_run_at; Type: COMMENT; Schema: public; Owner: user
+-- Name: COLUMN event.next_run_at; Type: COMMENT; Schema: public
 --
 
 COMMENT ON COLUMN public.event.next_run_at IS '事件下一次尝试的时间戳。';
 
 
 --
--- Name: COLUMN event.updated_at; Type: COMMENT; Schema: public; Owner: user
+-- Name: COLUMN event.updated_at; Type: COMMENT; Schema: public
 --
 
 COMMENT ON COLUMN public.event.updated_at IS '事件更新的时间戳。';
 
 
 --
--- Name: events_id_seq; Type: SEQUENCE; Schema: public; Owner: user
+-- Name: events_id_seq; Type: SEQUENCE; Schema: public
 --
 
 ALTER TABLE public.event ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
@@ -136,7 +130,7 @@ ALTER TABLE public.event ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
 
 
 --
--- Name: event events_pkey; Type: CONSTRAINT; Schema: public; Owner: user
+-- Name: event events_pkey; Type: CONSTRAINT; Schema: public
 --
 
 ALTER TABLE ONLY public.event
@@ -144,14 +138,14 @@ ALTER TABLE ONLY public.event
 
 
 --
--- Name: index_events_status_pending; Type: INDEX; Schema: public; Owner: user
+-- Name: index_events_status_pending; Type: INDEX; Schema: public
 --
 
 CREATE INDEX index_events_status_pending ON public.event USING btree (status, next_run_at);
 
 
 --
--- Name: event trigger_update_events_updated_at; Type: TRIGGER; Schema: public; Owner: user
+-- Name: event trigger_update_events_updated_at; Type: TRIGGER; Schema: public
 --
 
 CREATE TRIGGER trigger_update_events_updated_at BEFORE UPDATE ON public.event FOR EACH ROW EXECUTE FUNCTION public.update_updated_at_column();
