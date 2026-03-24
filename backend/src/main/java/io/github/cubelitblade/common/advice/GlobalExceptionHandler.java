035d754b5e902b0792abd4aa720083227f64a1f7
CubeLitBlade <135247075+CubeLitBlade@users.noreply.github.com>
2026-03-24 17:02:39 +0800

    feat(exception): add global exception handler
    - Handle InvalidParameterException via RestControllerAdvice
    - Return RFC 7807 ProblemDetail standard for error responses

---
+++ backend/src/main/java/io/github/cubelitblade/common/advice/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package io.github.cubelitblade.common.advice;
+
+import io.github.cubelitblade.common.exception.InvalidParameterException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(value = InvalidParameterException.class)
+    public ResponseEntity<ProblemDetail> handleIllegalArgument(InvalidParameterException e) {
+        ProblemDetail problem =  ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
+
+        if (e.getMessage() != null && !e.getMessage().isEmpty()) {
+            problem.setTitle(e.getMessage());
+        }
+
+        if (e.getDetail() != null && !e.getDetail().isEmpty()) {
+            problem.setDetail(e.getDetail());
+        }
+
+        return ResponseEntity.badRequest().body(problem);
+    }
+}
