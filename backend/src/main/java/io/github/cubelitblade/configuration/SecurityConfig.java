c09ef268907a2a178a3a0525dbb0a349c927214a
CubeLitBlade <135247075+CubeLitBlade@users.noreply.github.com>
2026-04-02 14:03:23 +0800

    fix(security): remove unnecessary exception declaration in filterChain method

--- backend/src/main/java/io/github/cubelitblade/configuration/SecurityConfig.java
+++ backend/src/main/java/io/github/cubelitblade/configuration/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http, JwtAuthenticationFilter jwtAuthenticationFilter) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http, JwtAuthenticationFilter jwtAuthenticationFilter) {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
