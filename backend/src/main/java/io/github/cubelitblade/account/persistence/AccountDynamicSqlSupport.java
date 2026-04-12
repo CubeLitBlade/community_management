5cc0664888a3bf4d13d551bcd286b65af469bcab
CubeLitBlade <135247075+CubeLitBlade@users.noreply.github.com>
2026-04-12 14:15:55 +0800

    feat(account): introduce dynamic SQL for multi-table joins
    Add basic dynamic SQL capabilities to the account persistence layer.
    Currently, this is primarily used to facilitate multi-table joins.
    Full dynamic SQL support will be implemented when the account query
    classes are refactored/reset later.

---
+++ backend/src/main/java/io/github/cubelitblade/account/persistence/AccountDynamicSqlSupport.java
@@ -0,0 +1,22 @@
+package io.github.cubelitblade.account.persistence;
+
+import java.sql.JDBCType;
+import org.mybatis.dynamic.sql.AliasableSqlTable;
+import org.mybatis.dynamic.sql.SqlColumn;
+
+public class AccountDynamicSqlSupport {
+  public static final Accounts accounts = new Accounts();
+  public static final SqlColumn<Long> id = accounts.id;
+  public static final SqlColumn<String> username = accounts.username;
+  public static final SqlColumn<String> nickname = accounts.nickname;
+
+  public static final class Accounts extends AliasableSqlTable<Accounts> {
+    public final SqlColumn<Long> id = column("id", JDBCType.BIGINT);
+    public final SqlColumn<String> username = column("username", JDBCType.VARCHAR);
+    public final SqlColumn<String> nickname = column("nickname", JDBCType.VARCHAR);
+
+    Accounts() {
+      super("account", Accounts::new);
+    }
+  }
+}
